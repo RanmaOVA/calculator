9ed7c410bcf4580acd76f26b5b64374224801b4f
RanmaOVA <Masseymichael643@gmail.com>
2022-02-07 17:37:21 -0600

    starting code

---
+++ calu.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionLister{
+
+    JFrame frame;
+    JTextField textfield;
+    JButton
+
+    Calculator(){
+        
+    }
+}
+
+public static void main(String[] args) {
+		
+    Calculator calc = new Calculator();
+}
