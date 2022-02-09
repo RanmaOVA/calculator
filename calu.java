e817e7b1a12db94ba4c9aa6a18d1ad92ef1384d9
RanmaOVA <Masseymichael643@gmail.com>
2022-02-08 23:16:27 -0600

    added structure

--- calu.java
+++ calu.java
@@ -12,7 +12,12 @@ public class Calculator implements ActionLister{
     JButton decButton, equButton, delButton, clrButton;
     JPanel panel;
 
-    Calculator(){
+    Font myFont = new Font("Ink Free",Font.BOLD,30);
+	
+	double num1=0,num2=0,result=0;
+	char operator;
+	
+	Calculator(){
 
     }
 
