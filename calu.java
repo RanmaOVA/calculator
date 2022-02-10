dcb015de6eff69015f03b7a140a12a976cafd763
RanmaOVA <Masseymichael643@gmail.com>
2022-02-09 22:05:46 -0600

    fixed bugs

--- calu.java
+++ calu.java
@@ -2,29 +2,29 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Calculator implements ActionLister{
+public class Calculator implements ActionListener{
 
-    JFrame frame;
-    JTextField textfield;
-    JButton[] numberButtons = new JButton[10];
-    JButton[] functionsButtons = new JButton[8];
-    JButton addButton,subButton,mulButton,divButton;
-    JButton decButton, equButton, delButton, clrButton;
-    JPanel panel;
-
-    Font myFont = new Font("Ink Free",Font.BOLD,30);
+	JFrame frame;
+	JTextField textfield;
+	JButton[] numberButtons = new JButton[10];
+	JButton[] functionButtons = new JButton[9];
+	JButton addButton,subButton,mulButton,divButton;
+	JButton decButton, equButton, delButton, clrButton, negButton;
+	JPanel panel;
+	
+	Font myFont = new Font("Ink Free",Font.BOLD,30);
 	
 	double num1=0,num2=0,result=0;
 	char operator;
 	
 	Calculator(){
-
-        frame = new JFrame("Calculator");
+		
+		frame = new JFrame("Calculator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(420, 550);
 		frame.setLayout(null);
-
-        textfield = new JTextField();
+		
+		textfield = new JTextField();
 		textfield.setBounds(50, 25, 300, 50);
 		textfield.setFont(myFont);
 		textfield.setEditable(false);
@@ -38,7 +38,7 @@ public class Calculator implements ActionLister{
 		delButton = new JButton("Del");
 		clrButton = new JButton("Clr");
 		negButton = new JButton("(-)");
-
+		
 		functionButtons[0] = addButton;
 		functionButtons[1] = subButton;
 		functionButtons[2] = mulButton;
@@ -62,11 +62,40 @@ public class Calculator implements ActionLister{
 			numberButtons[i].setFocusable(false);
 		}
 		
-
-    }
-
-    public static void main(String[] args) {
+		negButton.setBounds(50,430,100,50);
+		delButton.setBounds(150,430,100,50);
+		clrButton.setBounds(250,430,100,50);
 		
-          Calculator calc = new Calculator();
-    }
-}
+		panel = new JPanel();
+		panel.setBounds(50, 100, 300, 300);
+		panel.setLayout(new GridLayout(4,4,10,10));
+
+		panel.add(numberButtons[1]);
+		panel.add(numberButtons[2]);
+		panel.add(numberButtons[3]);
+		panel.add(addButton);
+		panel.add(numberButtons[4]);
+		panel.add(numberButtons[5]);
+		panel.add(numberButtons[6]);
+		panel.add(subButton);
+		panel.add(numberButtons[7]);
+		panel.add(numberButtons[8]);
+		panel.add(numberButtons[9]);
+		panel.add(mulButton);
+		panel.add(decButton);
+		panel.add(numberButtons[0]);
+		panel.add(equButton);
+		panel.add(divButton);
+		
+		frame.add(panel);
+		frame.add(negButton);
+		frame.add(delButton);
+		frame.add(clrButton);
+		frame.add(textfield);
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		Calculator calc = new Calculator();
+	}
